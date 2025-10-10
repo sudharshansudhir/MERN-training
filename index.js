f62bd65f7f5210cf52845d2d064efa5a3a0522bc
sudharshansudhir <sudhir.ssr7@gmail.com>
2025-10-10 11:58:08 +0530

    js objects

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-console.log("Hello World")
-console.log("Hello",5+2)
-console.log(`5+2 =${5+2}`)
+// console.log("Hello World")
+// console.log("Hello",5+2)
+// console.log(`5+2 =${5+2}`)
 
-var a=10;
-let b=20;
-const c=30; 
-a=40;
-b=50;
-// c=60; //error                
-console.log(a,b,c);
+// var a=10;
+// let b=20;
+// const c=30; 
+// a=40;
+// b=50;
+// // c=60; //error                
+// console.log(a,b,c);
 
-function add(x,y){
-    return x+y;
-}   
-console.log(add(5,10));
-console.log(add(5,"10"));
-console.log(add("5","10"));
-console.log(add(5));
-console.log(add());
-console.log(add(5,10,15,20));
+// function add(x,y){
+//     return x+y;
+// }   
+// console.log(add(5,10));
+// console.log(add(5,"10"));
+// console.log(add("5","10"));
+// console.log(add(5));
+// console.log(add());
+// console.log(add(5,10,15,20));
 
-function add2(x=0,y=0){
-    return x+y;
-}
+// function add2(x=0,y=0){
+//     return x+y;
+// }
 
-console.log(d)
-var d=100; //let and const will give error
-console.log(add2(5));
-console.log(add2());
-console.log(add2(5,10,15,20));
+// console.log(d)
+// var d=100; //let and const will give error
+// console.log(add2(5));
+// console.log(add2());
+// console.log(add2(5,10,15,20));
+let obj1={x:10};
+let obj2=obj1;
+obj2.x=20;
+console.log(obj1.x);
+console.log(obj2.x);
